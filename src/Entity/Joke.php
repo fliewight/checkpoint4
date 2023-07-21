8b2ad203d1aa5046a65424e03d33af27c4d25588
fliewight <lissjulien@gmail.com>
2023-07-21 10:28:17 +0200

    Ajout du nom de l'auteur de la blague

--- src/Entity/Joke.php
+++ src/Entity/Joke.php
@@ -20,6 +20,9 @@ class Joke
     #[ORM\ManyToOne]
     private ?Category $category = null;
 
+    #[ORM\ManyToOne]
+    private ?User $user = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -45,6 +48,18 @@ class Joke
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
