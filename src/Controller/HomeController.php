b662c11a85f351e80f4112b9ea1faa009b7538ab
fliewight <lissjulien@gmail.com>
2023-07-26 09:40:40 +0200

    Categories ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,20 +3,23 @@
 namespace App\Controller;
 
 use App\Repository\JokeRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/', name: 'app_')]
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(JokeRepository $jokeRepository): Response
+    public function index(JokeRepository $jokeRepository, CategoryRepository $categoryRepository): Response
     {
         $jokes = $jokeRepository->findBy([], null, 5);
+        $categories = $categoryRepository->findAll();
 
         return $this->render('home/index.html.twig', [
             'jokes' => $jokes,
+            'categories' => $categories,
         ]);
     }
 }
